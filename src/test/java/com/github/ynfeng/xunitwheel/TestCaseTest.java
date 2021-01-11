846d26a39b87a23145160fc818d4704472620877
ynfeng <fynwin@gmail.com>
2021-01-11 16:42:33 +0800

    重构，可以测试方法的运行顺序和名称.

--- src/test/java/com/github/ynfeng/xunitwheel/TestCaseTest.java
+++ src/test/java/com/github/ynfeng/xunitwheel/TestCaseTest.java
@@ -7,10 +7,20 @@ public class TestCaseTest {
 
         testMethods.run();
 
-        if (!testMethods.methodRun) {
+        if (!"method1".equals(testMethods.methodRunLog)) {
             throw new RuntimeException("method not run.");
         }
     }
+
+//    public void should_run_multi_test_method() {
+//        TestMethods testMethods = new TestMethods();
+//
+//        testMethods.run();
+//
+//        if (!testMethods.methodRun) {
+//            throw new RuntimeException("method not run.");
+//        }
+//    }
 
     public static void main(String[] args) {
         new TestCaseTest().should_run_test_method();
