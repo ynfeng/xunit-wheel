e9921b79de34eec295edf9f5969b1b75cb27a54f
ynfeng <fynwin@gmail.com>
2021-01-11 16:55:51 +0800

    可以注册测试方法

--- src/main/java/com/github/ynfeng/xunitwheel/OneTestMethodTestCase.java
+++ src/main/java/com/github/ynfeng/xunitwheel/OneTestMethodTestCase.java
@@ -1,10 +1,18 @@
 package com.github.ynfeng.xunitwheel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OneTestMethodTestCase {
     public String methodRunLog = "";
+    private final List<Runnable> testMethods = new ArrayList<>();
+
+    public OneTestMethodTestCase() {
+        testMethods.add(this::method1);
+    }
 
     public void run() {
-        method1();
+        testMethods.forEach(Runnable::run);
     }
 
     public void method1() {
